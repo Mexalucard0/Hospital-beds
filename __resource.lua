18ae54105df37c0d6608b3b31365c312b4c6a06d
Ramlov <mathias.ramlov10@gmail.com>
2019-07-06 05:29:33 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,6 @@
+-- Made by ramlov
+client_scripts {
+	'config.lua',
+	'client.lua',
+
+}
---
+++ config.lua
@@ -0,0 +1,86 @@
+Config              = {}
+Config.MaxDistance = 1.5
+--354.2610168457,-562.17199707031,29.717872619629
+--353.83923339844,-563.55572509766,28.79144859314
+--350.65707397461,-564.27404785156,28.791460037231
+--350.40957641602,-565.47149658203,28.79146194458
+Config.BedList = {
+
+	{
+		text = 'Tryk ~g~E~w~ for at lig på sengen',
+		heading = -20,
+		objCoords  = {x = 354.2610168457, y = -562.17199707031, z = 29.717872619629},--pillbox blue bed
+	},
+
+	{
+		text = 'Tryk ~g~E~w~ for at lig på sengen',
+		heading = -20.0,
+		objCoords  = {x = 360.51, y = -586.66, z = 43.11},--pillbox blue bed
+	},
+
+	{
+		text = 'Tryk ~g~E~w~ for at lig på sengen',
+		heading = -20.0,
+		objCoords  = {x = 353.12, y = -584.66, z = 43.50},---pillbox blue bed
+	},
+
+	{
+		text = 'Tryk ~g~E~w~ for at lig på sengen',
+		heading = -20.0,
+		objCoords  = {x = 349.62, y = -583.53, z = 43.022},---pillbox blue bed
+	},
+
+	{
+		text = 'Tryk ~g~E~w~ for at lig på sengen',
+		heading = 80.0,
+		objCoords  = {x = 344.80, y = -581.12, z = 43.02},---pillbox blue bed
+	},
+
+	{
+		text = 'Tryk ~g~E~w~ for at lig på sengen',
+		heading = 80.0,
+		objCoords  = {x = 334.09, y = -578.43, z = 43.01}, --pillbox blue bed
+	},
+
+	{
+		text = 'Tryk ~g~E~w~ for at lig på sengen',
+		heading = -20.0,
+		objCoords  = {x = 323.64, y = -575.16, z = 43.02}, --pillbox blue bed
+	},
+
+	{
+		text = 'Tryk ~g~E~w~ for at lig på sengen',
+		heading = -20.0,
+		objCoords  = {x = 326.97, y = -576.229, z = 43.02}, --pillbox blue bed
+	},
+
+	{
+		text = 'Tryk ~g~E~w~ for at lig på sengen',
+		heading = 160.0,
+		objCoords  = {x = 354.24, y = -592.74, z = 43.11}, --pillbox blue bed
+	},
+
+	{
+		text = 'Tryk ~g~E~w~ for at lig på sengen',
+		heading = 160.0,
+		objCoords  = {x = 357.34, y = -594.45, z = 43.11}, --pillbox bluebed
+	},
+
+	{
+		text = 'Tryk ~g~E~w~ for at lig på sengen',
+		heading = 160.0,
+		objCoords  = {x = 350.80, y = -591.72, z = 43.11}, --pillbox bluebed 
+	},
+
+	{
+		text = 'Tryk ~g~E~w~ for at lig på sengen',
+		heading = 160.0,
+		objCoords  = {x = 346.89, y = -591.01, z = 42.58},   --pillbox blue bed 
+	},
+
+	{
+		text = 'Tryk ~g~E~w~ for at lig på sengen',
+		heading = 80.0,
+		objCoords = {x = 326.98, y = -570.00, z = 43.01}, --pillbox blue bed surgery room
+	}
+}
